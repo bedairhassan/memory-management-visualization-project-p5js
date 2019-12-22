51ab9d69a5b429306390284d5f2b41aceea9c346
hankerhankk11 <55901623+hankerhankk11@users.noreply.github.com>
2019-12-22 15:51:26 +0200

    hankerhankk

---
+++ item3.js
@@ -0,0 +1,14 @@
+const start = {
+    x: 30,
+    y:30
+  }
+  
+  function setup() {
+    createCanvas(400, 400);
+   
+  }
+  
+  function draw() {
+    background(220);
+     rect(start.x, start.y, 80,20);
+  }
